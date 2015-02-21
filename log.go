ad3c180e777c133802b76d18eb8dc03c8537951d
Harrison <harrison@theshoebridges.com>
2015-02-22 00:12:38 +1100

    log: working on first refresh and maintain connections

--- log.go
+++ log.go
@@ -1,6 +1,7 @@
 package weblog
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -8,11 +9,15 @@ import (
 
 var upgrader = &websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
 
+func NewWebLog() WebLog {
+	return WebLog{make(chan []byte, 256)}
+}
+
 type WebLog struct {
 	send chan []byte
 }
 
-func (wl *WebLog) Write(p []byte) (int, error) {
+func (wl WebLog) Write(p []byte) (int, error) {
 	wl.send <- p
 	return len(p), nil
 }
@@ -23,12 +28,18 @@ func (wl WebLog) Handle(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for message := range wl.send {
-		err := ws.WriteMessage(websocket.TextMessage, message)
-		if err != nil {
-			break
-		}
-	}
+	ws.WriteMessage(websocket.TextMessage, []byte("Welcome to weblog!"))
 
-	ws.Close()
+	go func() {
+		for message := range wl.send {
+			err := ws.WriteMessage(websocket.TextMessage, message)
+			if err != nil {
+				fmt.Println("Error: ", err)
+				break
+			}
+			fmt.Println("SENDING MESSAGE")
+		}
+
+		ws.Close()
+	}()
 }
