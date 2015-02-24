576afc8b018bc87dea4e13b75eeb0949e5da2a73
Harrison <harrison@theshoebridges.com>
2015-02-24 20:25:44 +1100

    log: add documentation for public API

--- log.go
+++ log.go
@@ -10,6 +10,7 @@ import (
 
 var upgrader = &websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
 
+// NewWebLogger creates a new logger that can be used like a regular logger.
 func NewWebLogger() *WebLogger {
 	ww := &webWriter{register: make(chan *connection),
 		unregister:  make(chan *connection),
@@ -21,12 +22,14 @@ func NewWebLogger() *WebLogger {
 	return &WebLogger{l, ww}
 }
 
+// WebLogger allows users to work with a familiar API to work with when logging.
 type WebLogger struct {
 	*log.Logger
 
 	writer *webWriter
 }
 
+//Handle is the point used to intitiate the WebSocket connection
 func (wl *WebLogger) Handle(w http.ResponseWriter, r *http.Request) {
 	wl.writer.Handle(w, r)
 }
@@ -37,6 +40,7 @@ type webWriter struct {
 	connections map[*connection]bool
 }
 
+// Run manages all of the connections which need to be written to
 func (ww *webWriter) Run() {
 	for {
 		select {
@@ -51,6 +55,7 @@ func (ww *webWriter) Run() {
 	}
 }
 
+// Write satisfies the io.Writer interface used for logging.
 func (ww *webWriter) Write(p []byte) (int, error) {
 	for conn := range ww.connections {
 		conn.send <- p
@@ -59,11 +64,13 @@ func (ww *webWriter) Write(p []byte) (int, error) {
 	return len(p), nil
 }
 
+// Handle is the lowest level of WebLogger.Handle
 func (ww *webWriter) Handle(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		return
 	}
+
 	c := &connection{send: make(chan []byte, 256), ws: ws}
 	ww.register <- c
 	defer func() { ww.unregister <- c }()
